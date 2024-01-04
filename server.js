3fd90701106a9b84e497e6cd9dcfdcbf15663900
rrohann24 <rastogirohan.a@gmail.com>
2024-01-04 18:00:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require("dotenv");
+const {chats} = require("./data/data");
+const connectDB = require('./config/db');
+const colors = require("colors");
+const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
+const {notFound, errorHandler} = require("./middlewares/errorMiddleware")
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());
+// app.get('/', (req,res)=>{
+//     res.send("API is running bghg");
+// })
+app.use('/api/user',userRoutes);
+app.use('/api/chat',chatRoutes);
+app.use(notFound)
+app.use(errorHandler)
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,console.log(`server started at port ${PORT}`.bgMagenta));
